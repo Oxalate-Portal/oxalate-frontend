c732e9c27b167c0ee350139b5767f86238b3fc5c
Paul-Erik Törrönen <poltsi@poltsi.fi>
2024-09-30 21:15:13 +0300

    211: Show the preview image for certificates in the admin upload list
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/components/Administration/FileManagement/CertificateFiles.tsx
+++ src/components/Administration/FileManagement/CertificateFiles.tsx
@@ -29,7 +29,7 @@ export function CertificateFiles() {
             dataIndex: "certificateId",
             key: "certificateId",
         },
-        ...commonFileColumns({showPreview: false})
+        ...commonFileColumns({showPreview: true})
     ];
 
     return (
