42120fe7802f1884988cd820fbf1eb278b837b60
Paul-Erik Törrönen <poltsi@poltsi.fi>
2024-10-04 20:06:03 +0300

    211: Instead of dynamically update the portal configurations, show a button for each value that the user can press
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/components/Administration/PortalConfigurations.tsx
+++ src/components/Administration/PortalConfigurations.tsx
@@ -1,16 +1,16 @@
-import React, {useCallback, useEffect, useState} from "react";
-import {Button, Checkbox, Col, Divider, Input, InputNumber, message, Row, Space, Spin, Switch, Tooltip, Typography} from "antd";
-import {PortalConfigurationResponse} from "../../models/responses";
-import {portalConfigurationAPI} from "../../services";
-import {useTranslation} from "react-i18next";
-import {debounce} from "lodash";
+import React, { useEffect, useState } from "react";
+import { Button, Checkbox, Col, Divider, Input, InputNumber, message, Row, Space, Spin, Switch, Tooltip, Typography } from "antd";
+import { PortalConfigurationResponse } from "../../models/responses";
+import { portalConfigurationAPI } from "../../services";
+import { useTranslation } from "react-i18next";
 
-const { Text } = Typography;
+const {Text} = Typography;
 
 export function PortalConfigurations() {
     const [portalConfigurations, setPortalConfigurations] = useState<PortalConfigurationResponse[]>([]);
     const [loading, setLoading] = useState(true);
-    const { t } = useTranslation();
+    const [modifiedValues, setModifiedValues] = useState<Record<number, any>>({});
+    const {t} = useTranslation();
 
     const groupedConfigurations = portalConfigurations.reduce((acc, config) => {
         acc[config.groupKey] = acc[config.groupKey] || [];
@@ -37,76 +37,72 @@ export function PortalConfigurations() {
         return !value || /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
     }
 
-    const handleUpdate = useCallback(
-            (config: { type: string, id: number, value: any }) => {
-                if (config.type === "number") {
-                    config.value = Number(config.value);
-                } else if (config.type === "boolean") {
-                    config.value = config.value === "true";
-                }
+    function handleUpdate(configId: number) {
+        console.log("handleUpdate called with configId", configId);
+        const config = portalConfigurations.find((c) => c.id === configId);
 
-                if ((config.type === "string" || config.type === "email") && config.value === "") {
-                    config.value = null;
-                }
-
-                if (config.type === "email" && !validateEmail(config.value)) {
-                    message.error(t("PortalConfigurations.invalid-email"));
-                    return;
-                }
-
-                const updatedConfig = portalConfigurations.find((c) => c.id === config.id);
-
-                if (updatedConfig) {
-                    updatedConfig.runtimeValue = config.value;
-
-                    setLoading(true);
-
-                    portalConfigurationAPI.updateConfigurationValue({
-                        id: updatedConfig.id,
-                        value: config.value,
-                    })
-                            .then(() => {
-                                message.success(t("PortalConfigurations.update-ok"));
-                                reloadConfiguration();
-                            })
-                            .catch((error) => {
-                                console.error("Error updating configuration:", error);
-                                message.error(t("PortalConfigurations.update-fail"));
-                            })
-                            .finally(() => {
-                                setPortalConfigurations([...portalConfigurations]);
-                                setLoading(false);
-                            });
-                }
-            },
-            [portalConfigurations, t]
-    );
-
-    // Debounced update for string values
-    const debouncedUpdate = useCallback(debounce(handleUpdate, 1000), [handleUpdate]);
-
-    function renderEditor(config: PortalConfigurationResponse) {
-        const { valueType, runtimeValue, defaultValue } = config;
-
-        let selectedValue = defaultValue;
-
-        if (runtimeValue !== null && runtimeValue.length > 0) {
-            selectedValue = runtimeValue;
+        if (!config) {
+            console.error("Configuration not found");
+            return;
         }
 
-        const required = config.requiredRuntime && runtimeValue === null;
+        const valueType = config.valueType;
+        console.log("Found config", config);
+
+        const value = modifiedValues[configId];
+        console.log("Found value", value);
+
+        config.runtimeValue = value;
+
+        if (valueType === "email" && !validateEmail(value)) {
+            message.error(t("PortalConfigurations.invalid-email"));
+            return;
+        }
+
+        console.log("Updated configuration", config);
+
+        setLoading(true);
+
+        portalConfigurationAPI.updateConfigurationValue({
+            id: config.id,
+            value: config.runtimeValue,
+        })
+                .then(() => {
+                    message.success(t("PortalConfigurations.update-ok"));
+                    reloadConfiguration();
+                })
+                .catch((error) => {
+                    console.error("Error updating configuration:", error);
+                    message.error(t("PortalConfigurations.update-fail"));
+                })
+                .finally(() => {
+                    setPortalConfigurations([...portalConfigurations]);
+                    setLoading(false);
+                    setModifiedValues((prev) => ({...prev, [configId]: undefined}));
+                });
+    }
+
+    function renderEditor(config: PortalConfigurationResponse) {
+        const {valueType, runtimeValue, defaultValue} = config;
+
+        const currentValue = modifiedValues[config.id] !== undefined ? modifiedValues[config.id] : runtimeValue ?? defaultValue;
+        const required = config.requiredRuntime && currentValue === null;
+
+        function handleChange(newValue: any) {
+            setModifiedValues((prev) => ({...prev, [config.id]: newValue}));
+            console.log("ModifiedValues:", modifiedValues);
+        }
 
         switch (valueType) {
             case "array":
                 const options = defaultValue.split(",");
-                const selectedValues = selectedValue.split(",");
+                const selectedValues = currentValue.split(",");
                 return (
                         <>
                             <Checkbox.Group
                                     options={options}
-                                    key={valueType + "-" + config.id}
                                     value={selectedValues}
-                                    onChange={(checkedValues) => handleUpdate({ type: config.valueType, id: config.id, value: checkedValues.join(",") })}
+                                    onChange={(checkedValues) => handleChange(checkedValues.join(","))}
                             />
                             {required && <Text type="danger">{t("PortalConfigurations.must-be-set")}</Text>}
                         </>
@@ -118,9 +114,8 @@ export function PortalConfigurations() {
                             <Switch
                                     checkedChildren={t("common.button.yes")}
                                     unCheckedChildren={t("common.button.no")}
-                                    checked={selectedValue === "true"}
-                                    key={valueType + "-" + config.id}
-                                    onChange={(checked) => handleUpdate({ type: config.valueType, id: config.id, value: checked ? "true" : "false" })}
+                                    checked={currentValue === "true"}
+                                    onChange={(checked) => handleChange(checked ? "true" : "false")}
                             />
                             {required && <Text type="danger">{t("PortalConfigurations.must-be-set")}</Text>}
                         </>
@@ -130,9 +125,8 @@ export function PortalConfigurations() {
                 return (
                         <>
                             <Input
-                                    defaultValue={selectedValue}
-                                    key={valueType + "-" + config.id}
-                                    onChange={(e) => debouncedUpdate({ type: config.valueType, id: config.id, value: e.target.value })}
+                                    value={currentValue}
+                                    onChange={(e) => handleChange(e.target.value)}
                             />
                             {required && <Text type="danger">{t("PortalConfigurations.must-be-set")}</Text>}
                         </>
@@ -142,9 +136,8 @@ export function PortalConfigurations() {
                 return (
                         <>
                             <InputNumber
-                                    value={Number(selectedValue)}
-                                    key={valueType + "-" + config.id}
-                                    onChange={(value) => debouncedUpdate({ type: config.valueType, id: config.id, value: value?.toString() })}
+                                    value={Number(currentValue)}
+                                    onChange={(value) => handleChange(value?.toString())}
                             />
                             {required && <Text type="danger">{t("PortalConfigurations.must-be-set")}</Text>}
                         </>
@@ -154,9 +147,8 @@ export function PortalConfigurations() {
                 return (
                         <>
                             <Input
-                                    defaultValue={selectedValue}
-                                    key={valueType + "-" + config.id}
-                                    onChange={(e) => debouncedUpdate({ type: config.valueType, id: config.id, value: e.target.value })}
+                                    value={currentValue}
+                                    onChange={(e) => handleChange(e.target.value)}
                             />
                             {required && <Text type="danger">{t("PortalConfigurations.must-be-set")}</Text>}
                         </>
@@ -165,10 +157,6 @@ export function PortalConfigurations() {
             default:
                 return null;
         }
-    }
-
-    function capitalizeFirstLetter(string: string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
     function reloadConfiguration() {
@@ -201,38 +189,32 @@ export function PortalConfigurations() {
                         <div>
                             {!loading && Object.entries(groupedConfigurations).map(([groupKey, configs]) => (
                                     <div key={groupKey}>
-                                        <Divider orientation="left">{capitalizeFirstLetter(groupKey)}</Divider>
-                                        <Row gutter={16}
-                                             style={{ marginBottom: "16px", backgroundColor: "rgba(0, 0, 0, 0.75)", padding: "8px", borderRadius: "4px" }}>
-                                            <Col span={6}>
-                                                <Text strong>{t("PortalConfigurations.configuration")}</Text>
-                                            </Col>
-                                            <Col span={6}>
-                                                <Text strong>{t("PortalConfigurations.default-value")}</Text>
-                                            </Col>
-                                            <Col span={6}>
-                                                <Text strong>{t("PortalConfigurations.current-setting")}</Text>
-                                            </Col>
-                                        </Row>
-                                        {configs
-                                                .sort((a, b) => a.settingKey.localeCompare(b.settingKey)) // Sort the settingKeys alphabetically
-                                                .map(config => (
-                                                        <div key={config.id} style={{ marginBottom: "16px" }}>
-                                                            <Row gutter={16}>
-                                                                <Col span={6}>
-                                                                    <Tooltip title={t("PortalConfigurations." + config.groupKey + "." + config.settingKey + ".tooltip")}>
-                                                                        <Text strong>{t("PortalConfigurations." + config.groupKey + "." + config.settingKey + ".label")}</Text>
-                                                                    </Tooltip>
-                                                                </Col>
-                                                                <Col span={6}>
-                                                                    <Text>{config.defaultValue}</Text>
-                                                                </Col>
-                                                                <Col span={6}>
-                                                                    {renderEditor(config)}
-                                                                </Col>
-                                                            </Row>
-                                                        </div>
-                                                ))}
+                                        <Divider orientation="left">{groupKey}</Divider>
+                                        {configs.sort((a, b) => a.settingKey.localeCompare(b.settingKey)).map(config => (
+                                                <div key={config.id} style={{marginBottom: "16px"}}>
+                                                    <Row gutter={16}>
+                                                        <Col span={6}>
+                                                            <Tooltip
+                                                                    title={t("PortalConfigurations." + config.groupKey + "." + config.settingKey + ".tooltip")}>
+                                                                <Text strong>{t("PortalConfigurations." + config.groupKey + "." + config.settingKey + ".label")}</Text>
+                                                            </Tooltip>
+                                                        </Col>
+                                                        <Col span={6}>
+                                                            <Text>{config.defaultValue}</Text>
+                                                        </Col>
+                                                        <Col span={6}>
+                                                            {renderEditor(config)}
+                                                        </Col>
+                                                        <Col span={6}>
+                                                            {modifiedValues[config.id] !== undefined && (
+                                                                    <Button type="primary" onClick={() => handleUpdate(config.id)}>
+                                                                        {t("common.button.update")}
+                                                                    </Button>
+                                                            )}
+                                                        </Col>
+                                                    </Row>
+                                                </div>
+                                        ))}
                                     </div>
                             ))}
                         </div>
