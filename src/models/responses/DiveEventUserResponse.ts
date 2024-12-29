0be018b05dacfcfe93c90ed2432da95341d6c207
Paul-Erik Törrönen <poltsi@poltsi.fi>
2024-12-29 22:06:58 +0200

    232: Add createdAt to DiveEventUserResponse.ts to show when the diver has clicked to participate to the dive event
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- src/models/responses/DiveEventUserResponse.ts
+++ src/models/responses/DiveEventUserResponse.ts
@@ -1,8 +1,10 @@
+import {Dayjs} from "dayjs";
 import {PaymentResponse} from "./PaymentResponse";
 
 export interface DiveEventUserResponse {
     id: number;
     name: string;
     eventDiveCount: number;
+    createdAt: Dayjs;
     payments: PaymentResponse[];
 }
